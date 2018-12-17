a32a5e1b54c9b02fe24fae93e2e78632f77be82a
Volodymyr Flyonts <flyonts@gmail.com>
2018-12-17 20:31:07 +0200

    add tender_queue and plan_queue plugins

---
+++ openprocurement/search_plugins/plan_queue.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+from logging import getLogger
+
+from openprocurement.search.base_plugin import BasePlugin
+from openprocurement.search.utils import get_now, long_version
+
+logger = getLogger(__name__)
+
+
+class SearchPlugin(BasePlugin):
+    __name__ = __name__
+    plugin_api_version = 2.0
+
+    config = {
+        'plan_queue_file': '',
+    }
+
+    def __init__(self, config):
+        for k in self.config.keys():
+            if k in config:
+                self.config[k] = config[k]
+
+    def before_source_items(self, index):
+        preload_items = []
+        if self.config['plan_queue_file'] and os.path.exists(self.config['plan_queue_file']):
+            logger.info("[plan_queue] Found queue file %s", self.config['plan_queue_file'])
+            for line in open(self.config['plan_queue_file']):
+                line = line.strip()
+                if len(line) == 32:
+                    preload_items.append({'id': line, 'dateModified': get_now().isoformat(), 'ignore_dateModified': 1})
+
+            os.rename(self.config['plan_queue_file'], self.config['plan_queue_file'] + ".old")
+            if preload_items:
+                logger.info("[plan_queue] %d plan ids loaded from file", len(preload_items))
+
+        return preload_items
